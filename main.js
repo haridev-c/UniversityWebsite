1cddf5fa9f7b6ad370670eb2d2aaf36205179c27
Haridev <haridev.8281@outlook.com>
2024-09-07 13:43:28 +0530

    fixed issues with importing javascript

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-function showMenu() {
-  document.getElementById("logslink").style.right = "0";
-}
+// export function showMenu() {
+//   document.getElementById("logslink").style.right = "0";
+// }
 
-function hideMenu() {
+// export function hideMenu() {
+//   document.getElementById("logslink").style.right = "-100%";
+// }
+
+const hamburgerMenu = document.querySelector("#hamburgerMenu");
+const closeMenu = document.querySelector("#menuClose");
+
+hamburgerMenu.addEventListener("click", () => {
+  document.getElementById("logslink").style.right = "0";
+});
+
+closeMenu.addEventListener("click", () => {
   document.getElementById("logslink").style.right = "-100%";
-}
+});
